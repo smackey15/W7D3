e2ec6ee47da7a0bb772b14976eb78339326cf5ae
sean <midruss@yahoo.com>
2022-01-19 15:47:04 -0500

    wrote specs for user model methods

--- Capybara/db/schema.rb
+++ Capybara/db/schema.rb
@@ -16,8 +16,12 @@ ActiveRecord::Schema.define(version: 2022_01_19_191257) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end
--- Capybara/spec/models/user_spec.rb
+++ Capybara/spec/models/user_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  #subject(:cathy) { User.create(username: "cathy", password: "password") }
+  subject(:cathy) { User.new(username: "cathy", password: "password") }
+  #before(:each) do 
+  #  create(:user)
+  #end
 
   it { should validate_presence_of(:username)}
   it { should validate_presence_of(:passowrd_digest)}
@@ -17,8 +20,8 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:session_token)}
   end
   #pending "add some examples to (or delete) #{__FILE__}"
-  subject(:user) do User.new(username: "Joey", pasword: "password")
-  end 
+  #subject(:user) do User.new(username: "Joey", password: "password")
+  #end 
   describe ".find_by_credentials" do
     before {user.save!}
     it "return the user if given good credentials" do
@@ -27,6 +30,42 @@ RSpec.describe User, type: :model do
     it "return nil if given bad credentials" do
       expect(User.find_by_credentials("Joey", "bad_password")).to eq(nil)
     end
-  end 
+  end
+  
+  describe 'check_password?' do
+    let!(:user) { create(:user) } # "You can use let! to force the method's invocation before each example."
+    # https://relishapp.com/rspec/rspec-core/v/2-11/docs/helper-methods/let-and-let
+
+    context "with a valid password" do
+        it "should return true" do
+            expect(user.check_password?("password")).to be true
+        end
+    end
+
+    context "with invalid password" do
+        it "should return false" do
+            expect(user.check_password?("bananaman")).to be false
+        end
+    end
 end
 
+describe 'password incryption' do
+
+    it 'does not save password to the database' do
+        # FactoryBot.create(:user, username: 'Harry Potter')
+        FactoryBot.create(:harry_potter) 
+        # create "Returns a saved User instance"
+
+        user = User.find_by(username: 'Harry Potter')
+        expect(user.password).not_to eq('password')
+    end
+
+    it 'encrypts password using BCrypt' do 
+        expect(BCrypt::Password).to receive(:create).with('abcdef')
+
+        FactoryBot.build(:user, password: 'abcdef')
+        # Returns a User instance that's not saved
+        # https://www.rubydoc.info/gems/factory_bot/file/GETTING_STARTED.md#build-strategies
+    end
+end
+end
