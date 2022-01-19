e561890d759c33093ed0e3f0824a1cc4e9deee61
Deanding <dingtian.ding@gmail.com>
2022-01-19 15:21:02 -0500

    wrote find_by_credentials and troubleshoot with Evan

--- Capybara/spec/factories/users.rb
+++ Capybara/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
-
+    username {Faker::Movies::Departed.character}
+    password {'password'}
 
     
   end
--- Capybara/spec/models/user_spec.rb
+++ Capybara/spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  #subject(:cathy) { User.create(username: "cathy", password: "password") }
+
   it { should validate_presence_of(:username)}
   it { should validate_presence_of(:passowrd_digest)}
   it { should validate_length_of(:password).is_at_least(6)}
@@ -14,5 +17,16 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:session_token)}
   end
   #pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) do User.new(username: "Joey", pasword: "password")
+  end 
+  describe ".find_by_credentials" do
+    before {user.save!}
+    it "return the user if given good credentials" do
+      expect(User.find_by_credentials("Joey", "password")).to eq(user)
+    end
+    it "return nil if given bad credentials" do
+      expect(User.find_by_credentials("Joey", "bad_password")).to eq(nil)
+    end
+  end 
 end
 
